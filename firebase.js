a7e2f1fe3243dfc7e391f92315f95ea1b727a866
carloposadino-crypto <carloposadino@gmail.com>
2026-09-09 00:20:59 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,16 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Sostituisci i valori con i dati del tuo progetto Firebase Console
 const firebaseConfig = {
-  authDomain: "haccpcava.firebaseapp.com",
-  projectId: "haccpcava",
-  storageBucket: "haccpcava.appspot.com"
+  apiKey: "TUA_API_KEY",
+  authDomain: "TUO_PROJECT_ID.firebaseapp.com",
+  projectId: "TUO_PROJECT_ID",
+  storageBucket: "TUO_PROJECT_ID.appspot.com",
+  messagingSenderId: "TUO_SENDER_ID",
+  appId: "TUO_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export { collection, addDoc, getDocs, query, where, serverTimestamp };
