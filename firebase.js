0f57fdc8687a2ef85a4fb656385df001f563fcd4
carloposadino-crypto <carloposadino@gmail.com>
2026-09-09 11:11:34 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, query, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8g8CQ9z7EOfRj4CkUgscMquB7uXwIuGA",
-  authDomain: "haccpcava.firebaseapp.com",
-  projectId: "haccpcava",
-  storageBucket: "haccpcava.firebasestorage.app",
-  messagingSenderId: "569829408620",
-  appId: "1:569829408620:web:d190af863d11b45644fd49"
+  apiKey: "TUA_API_KEY",
+  authDomain: "TUO_DOMINIO.firebaseapp.com",
+  projectId: "TUO_PROJECT_ID",
+  storageBucket: "TUO_STORAGE_BUCKET",
+  messagingSenderId: "TUO_MESSAGING_SENDER_ID",
+  appId: "TUO_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { collection, addDoc, getDocs, query, where, serverTimestamp };
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, query, where, serverTimestamp };
