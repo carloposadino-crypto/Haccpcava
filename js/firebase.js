c73d67e5dd11b6153b9cfe04a0290b97793a18bc
carloposadino-crypto <carloposadino@gmail.com>
2026-09-12 12:19:12 +0200

    Export deleteDoc in firebase.js e fix ricette.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,25 +4,25 @@ import {
   collection, 
   addDoc, 
   getDocs, 
+  doc, 
+  deleteDoc, 
   query, 
   orderBy, 
-  limit, 
   serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { firebaseConfig } from './config.js';
 
-// Configurazioni dirette del progetto Firebase
-const firebaseConfig = {
-  apiKey: "INSERISCI_QUI_LA_TUA_API_KEY",
-  authDomain: "haccpcava.firebaseapp.com",
-  projectId: "haccpcava",
-  storageBucket: "haccpcava.appspot.com",
-  messagingSenderId: "INSERISCI_QUI_SENDER_ID",
-  appId: "INSERISCI_QUI_APP_ID"
-};
-
-// Inizializzazione di Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Esportiamo il database e le funzioni utili per le altre sezioni dell'app
-export { db, collection, addDoc, getDocs, query, orderBy, limit, serverTimestamp };
+export { 
+  db, 
+  collection, 
+  addDoc, 
+  getDocs, 
+  doc, 
+  deleteDoc, 
+  query, 
+  orderBy, 
+  serverTimestamp 
+};
