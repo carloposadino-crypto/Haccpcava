378b8e3e85291123301975cc3d69ab121ff7aacf
carloposadino-crypto <carloposadino@gmail.com>
2026-09-15 10:30:09 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,43 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-  getFirestore, 
-  collection, 
-  getDocs, 
-  addDoc, 
-  query, 
-  where, 
-  orderBy, 
-  limit, 
-  doc, 
-  updateDoc, 
-  deleteDoc, 
-  serverTimestamp 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// Sostituisci con le tue credenziali Firebase se diverse
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyC8g8CQ9z7E0fRj4CKugscMquB7uXwIuGA",
   authDomain: "haccpcava.firebaseapp.com",
   projectId: "haccpcava",
-  storageBucket: "haccpcava.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { 
-  db, 
-  collection, 
-  getDocs, 
-  addDoc, 
-  query, 
-  where, 
-  orderBy, 
-  limit, 
-  doc, 
-  updateDoc, 
-  deleteDoc, 
-  serverTimestamp 
+  storageBucket: "haccpcava.firebasestorage.app",
+  messagingSenderId: "569829408620",
+  appId: "1:569829408620:web:d190af863d11b45644fd49"
 };
